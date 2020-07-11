d5b7c0fd565c5edc20735c1ee77aefaf7d66df63
buithikieu11 <buithikieu16tclc3@gmail.com>
2020-07-11 19:54:12 +0700

    Course_Validation

--- db/migrate/20200710020430_create_courses.rb
+++ db/migrate/20200710020430_create_courses.rb
@@ -1,7 +1,7 @@
 class CreateCourses < ActiveRecord::Migration[6.0]
   def change
     create_table :courses do |t|
-      t.string :name, null: false
+      t.string :name, null: false ,unique: true
       t.text :description
       t.text :image
       t.integer :status, default: 0, null: false
