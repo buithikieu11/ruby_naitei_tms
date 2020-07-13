1a47e72cf90b028e0a09b1dc63ea78c83f496da2
Doan Huu Hoa <doan.huu.hoa@sun-asterisk.com>
2020-07-13 16:22:12 +0700

    Finish fixing Rubocop Convention mistake

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,13 +1,15 @@
 class Course < ApplicationRecord
-    enum status: {pending: 0, started: 1, finish: 2}
-    validates :name, uniqueness: { case_sensitive: false }, presence: true,
-    length: { maximum: Settings.model.course.name.max_length }, format: { with: /\A[a-zA-Z0-9 ]+\z/ }
-    validates :description, presence: true
-    validates :status, presence: true, inclusion: { in: statuses.keys }
-    validates :day_start, presence: true
-    validate  :day_start_is_lower_than_day_end
-    
-    def day_start_is_lower_than_day_end
-      errors.add(:invalid_date, "day_start_is_lower_than_day_end") if day_start >= day_end 
-    end
+  enum status: {pending: 0, started: 1, finish: 2}
+  validates :name, uniqueness: {case_sensitive: false},
+                   presence: true,
+                   length: {maximum: Settings.model.course.name.max_length},
+                   format: {with: /\A[a-zA-Z0-9 ]+\z/}
+  validates :description, presence: true
+  validates :status, presence: true, inclusion: {in: statuses.keys}
+  validates :day_start, presence: true
+  validate  :day_start_is_lower_than_day_end
+
+  def day_start_is_lower_than_day_end
+    errors.add(:invalid_date, "day_start_is_lower_than_day_end") if day_start >= day_end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   enum role: {trainee: 0, supervisor: 1}
 
+  MODEL_SETTINGS = Settings.model.user
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   VALID_PHONE_NUMBER_REGEX = /\A\+?[0-9]+\z/.freeze
   VALID_USERNAME_REGEX = /\A[a-zA-Z0-9]+\z/.freeze
@@ -8,16 +9,16 @@ class User < ApplicationRecord
   validates :role, numericality: true, inclusion: {in: roles}
   validates :username, presence: true,
                        uniqueness: {case_sensitive: false},
-                       length: {minimum: Settings.model.user.username.min_length,
-                                maximum: Settings.model.user.username.max_length},
+                       length: {minimum: MODEL_SETTINGS.username.min_length,
+                                maximum: MODEL_SETTINGS.username.max_length},
                        format: {with: VALID_USERNAME_REGEX}
   validates :password, presence: true
   validates :email, presence: true,
                     uniqueness: {case_sensitive: false},
-                    length: {maximum: Settings.model.user.email.max_length},
+                    length: {maximum: MODEL_SETTINGS.email.max_length},
                     format: {with: VALID_EMAIL_REGEX}
-  validates :phone_number, length: {minimum: Settings.model.user.phone_number.min_length,
-                                    maximum: Settings.model.user.phone_number.max_length},
+  validates :phone_number, length: {minimum: MODEL_SETTINGS.phone_number.min_length,
+                                    maximum: MODEL_SETTINGS.phone_number.max_length},
                            format: {with: VALID_PHONE_NUMBER_REGEX}
 
   has_secure_password
