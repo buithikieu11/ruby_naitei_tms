4bad561db15d92ad67132f5d0cf5825677a9a6f7
minhtam2048 <minhtam2048@gmail.com>
2020-07-14 16:47:06 +0700

    Fix Remember User Feature

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -20,7 +20,7 @@ module SessionsHelper
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
-      if user&.authenticated?(cookies[:remember_token])
+      if user&.authenticated?(:remember, cookies[:remember_token])
         log_in user
         @current_user = user
       end
@@ -30,10 +30,12 @@ module SessionsHelper
   def logged_in?
     current_user.present?
   end
-
+  
   def log_out
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
+
+
 end
