f8fba5ffb6b2137ca747f121440dfe469c0ceb85
minhtam2048 <minhtam2048@gmail.com>
2020-07-28 15:19:44 +0700

    Add Unit Test To Model User

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,10 +1,21 @@
+require "faker"
+
 FactoryBot.define do
-  factory :user, class: User do
+  factory :user do
     username                { Faker::Internet.unique.username(specifier: 7..9) }
     email                   { Faker::Internet.unique.email }
     password                { "123456" }
     password_confirmation   { "123456" }
-    phone_number            { Faker::Number.number(12)}
+    phone_number            { Faker::Number.number(digits: 12)}
+    department              { Faker::Company.industry }
+  end
+
+  factory :invalid_user, class: User do
+    username                { Faker::Internet.unique.username(specifier: 20..30) }
+    email                   { Faker::Internet.unique.username }
+    password                { "123456" }
+    password_confirmation   { "" }
+    phone_number            { Faker::Number.number(digits: 20)}
     department              { Faker::Company.industry }
   end
 end
