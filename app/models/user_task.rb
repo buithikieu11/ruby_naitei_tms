790154af2b44c7426811528c084e49b8e552949a
Doan Huu Hoa <doan.huu.hoa@sun-asterisk.com>
2020-08-05 13:03:21 +0700

    Finish Subject/Show page

--- app/models/user_task.rb
+++ app/models/user_task.rb
@@ -1,4 +1,7 @@
 class UserTask < ApplicationRecord
   belongs_to :user
   belongs_to :task
+
+  scope :where_in, ->(column, array){where("#{column} in (?)", array)}
+  scope :find_all_by_column, ->(column, value){where("#{column} = #{value}")}
 end
